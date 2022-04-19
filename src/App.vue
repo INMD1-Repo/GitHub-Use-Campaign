5de5298bf305c74281446769ab928662caa4a1bb
INMD1 <87979171+INMD1@users.noreply.github.com>
2022-04-19 15:04:47 +0900

    깃허브 업로드

--- src/App.vue
+++ src/App.vue
@@ -61,81 +61,76 @@
     </article>
     <!-- What is Github? -->
     <article id="main" class="what_color c_article">
-      <div class="py-5 text-center align-items-center d-flex">
-        <div class="container py-4 py-xl-5">
-          <div class="row mb-5">
-            <div class="col-md-8 col-xl-6 text-center py-deit mx-auto">
-              <h2 class="what_main_title">What is GitHub?</h2>
-              <h2 class="what_main_sub_title">깃허브는 대체 무엇일까?</h2>
+      <div data-aos="fade-up">
+        <div class="py-5 text-center align-items-center d-flex">
+          <div class="text-center py-deit mx-auto">
+            <h2 class="what_main_title_white">What is GitHub?</h2>
+            <h2 class="what_main_sub_title_white">깃허브는 대체 무엇일까?</h2>
+          </div>
+        </div>
+      </div>
+      <div data-aos="fade-up" data-aos-delay="4000">
+        <div class="row what_row">
+          <div class="col-md-6 col-xl-4 mb-4">
+            <div class="p-xl-3 m-xl-3">
+              <img
+                class="what_img rounded img-fluid w-100"
+                src="./assets/img/git_github.png"
+              />
             </div>
           </div>
-          <div class="row row-cols-1 row-cols-md-2">
-            <div class="col mb-4">
-              <div class="d-flex flex-column flex-lg-row">
-                <div class="w-100">
-                  <img class="what_sub_img" src="./assets/img/git_github.png" />
-                </div>
-                <div class="py-4 py-lg-0 px-lg-4">
-                  <h4 class="what_title">GitHub X Git</h4>
-                  <p>
-                    깃허브는 분산 버전 컨트롤 소프트웨어 깃(Git)을 기반으로 소스
-                    코드를 호스팅하고 협업 지원 기능들을 지원하는 웹서비스
-                    입니다.
-                  </p>
-                </div>
-              </div>
+          <div class="col-md-6 d-md-flex align-items-md-center mb-4">
+            <div style="max-width: 500px">
+              <h2 class="what_col_title_white">GitHub X Git</h2>
+              <p class="what_text_body_white my-3">
+                깃허브는 분산 버전 컨트롤 소프트웨어 깃(Git)을 기반으로 소스
+                코드를 호스팅하고 협업 지원 기능들을 지원하는 웹서비스 입니다.
+              </p>
             </div>
-            <div class="col mb-4">
-              <div class="d-flex flex-column flex-lg-row">
-                <div class="w-100">
-                  <img class="what_sub_img" src="./assets/img/opensouce.jpg" />
-                </div>
-                <div class="py-4 py-lg-0 px-lg-4">
-                  <h4 class="what_title">OpenSource</h4>
-                  <p>
-                    깃허브는 개발자들이 많이 쓰는 사이트입니다. 그러다 보니
-                    개발자들이 소스를 많이 사람들이 쓰거나 볼수 있게 오픈소스로
-                    많이 공개합니다.
-                  </p>
-                </div>
-              </div>
+          </div>
+        </div>
+      </div>
+      <div data-aos="fade-up" data-aos-delay="4000">
+        <div class="row what_row d-flex flex-row-reverse">
+          <div class="col-md-6 col-xl-4 mb-4">
+            <div class="p-xl-3 m-xl-3">
+              <img
+                class="what_img rounded img-fluid w-100"
+                src="./assets/img/opensouce.jpg"
+              />
             </div>
-            <div class="col mb-4">
-              <div class="d-flex flex-column flex-lg-row">
-                <div class="w-100">
-                  <img
-                    class="what_sub_img_large"
-                    src="./assets/img/wallpaper.jpg"
-                  />
-                </div>
-                <div class="py-4 py-lg-0 px-lg-4">
-                  <h4 class="what_title">Code management</h4>
-                  <p>
-                    위에 말한거 같이 깃허브는 깃이라는 프로그램을 쓰기 때문에
-                    코드를 올릴떄 마다 변경점을 찾을수 있어서 코드 관리에 매우
-                    유용합니다.
-                  </p>
-                </div>
-              </div>
+          </div>
+          <div
+            class="col-md-6 d-flex flex-row-reverse align-items-md-center mb-4"
+          >
+            <div style="max-width: 500px">
+              <h2 class="what_col_title_white">OpenSource</h2>
+              <p class="what_text_body_white my-3">
+                깃허브는 개발자들이 많이 쓰는 사이트입니다.
+                그래서 코드나 아이더가 풍부하고 일부 코드는 오픈소스로 풀어져 있어서 
+                소스를 보거나 참조할수 있습니다.
+              </p>
             </div>
-            <div class="col mb-4">
-              <div class="d-flex flex-column flex-lg-row">
-                <div class="w-100">
-                  <img
-                    class="rounded img-fluid w-100 fit-cover"
-                    src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png"
-                    style="height: 200px"
-                  />
-                </div>
-                <div class="py-4 py-lg-0 px-lg-4">
-                  <h4 class="font-weight-bold">Lorem libero donec</h4>
-                  <p>
-                    Nullam id dolor id nibh ultricies vehicula ut id elit. Cras
-                    justo odio, dapibus ac facilisis in, egestas eget quam.
-                    Donec id elit non mi porta gravida at eget metus.
-                  </p>
-                </div>
-              </div>
+          </div>
+        </div>
+      </div>
+      <div data-aos="fade-up" data-aos-delay="4000">
+        <div class="row what_row">
+          <div class="col-md-6 col-xl-4 mb-4">
+            <div class="p-xl-3 m-xl-3">
+              <img
+                class="what_img rounded img-fluid w-100"
+                src="./assets/img/wallpaper.jpg"
+              />
+            </div>
+          </div>
+          <div class="col-md-6 d-md-flex align-items-md-center mb-4">
+            <div style="max-width: 500px">
+              <h2 class="what_col_title_white">Code management</h2>
+              <p class="what_text_body_white my-3">
+                위에 말한거 같이 깃허브는 깃이라는 프로그램을 쓰기 때문에 코드를
+                올릴떄 마다 변경점을 찾을수 있어서 코드 관리에 매우 유용합니다.
+              </p>
             </div>
           </div>
         </div>
@@ -146,8 +141,11 @@
         <div class="container py-4 py-xl-5">
           <div class="row mb-4">
             <div class="col-md-8 col-xl-8 text-center mx-auto py-deit">
-              <p class="what_main_title_white">Why Use Github?<p>
-              <h3 class="what_main_sub_title_white">우리가 깃허브를 이용하면 이런것들을 이용할수 있습니다.</h3>
+              <p class="what_main_title_white">Why Use Github?</p>
+              <p></p>
+              <h3 class="what_main_sub_title_white">
+                우리가 깃허브를 이용하면 이런것들을 이용할수 있습니다.
+              </h3>
             </div>
           </div>
           <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3">
@@ -159,14 +157,17 @@
                 />
                 <div class="card-body p-4">
                   <p class="text-primary mb-0">개발도구</p>
-                  <h4 class="font-weight-bold card-title">
-                    Github actions
-                  </h4>
+                  <h4 class="font-weight-bold card-title">Github actions</h4>
                   <p class="card- text">
-                    깃허브 액션은 빌드, 테스트 및 배포 파이프라인을 자동화 할수 있는 CL/CD입니다.
-                    그래서 이걸 이용해서 파일을 배포가 가능하고 Gihub Page에 사이트를 배포 할수 있습니다.
+                    깃허브 액션은 빌드, 테스트 및 배포 파이프라인을 자동화 할수
+                    있는 CL/CD입니다. 그래서 이걸 이용해서 파일을 배포가
+                    가능하고 Gihub Page에 사이트를 배포 할수 있습니다.
                   </p>
-                  <button type="button" class="btn btn-primary btn-lg">페이지 방문</button>
+                  <a
+                    href="https://github.com/features/actions"
+                    class="btn btn-secondary"
+                    ><i class="bi bi-github"></i> 페이지 방문</a
+                  >
                 </div>
               </div>
             </div>
@@ -182,9 +183,23 @@
                     Github Student Pack
                   </h4>
                   <p class="card-text">
-                    깃허브 학생 팩은 학생이 재학동안 IDE, 도메인등 무료로 제공하는 패키지입니다. 
-                    이걸 사용하는 방법은 먼저 대학교 이메일을 통해서 인증하면 됨니다.
+                    깃허브 학생 팩은 학생이 재학동안 IDE, 도메인등 프로그램을
+                    무료로 제공하는 패키지입니다. 이걸 사용하는 방법은 먼저
+                    대학교 이메일을 통해서 인증하면 됨니다.
                   </p>
+                  <div class="d-grid gap-2 d-lg-flex justify-content-center">
+                    <a
+                      href="https://education.github.com/pack"
+                      class="btn btn-secondary"
+                      ><i class="bi bi-github"></i> 페이지 방문</a
+                    >
+                    <a
+                      href="https://education.github.com/discount_requests/student_application"
+                      class="btn"
+                      style="background-color: #79dfc1"
+                      >대학교 이메일 인증하기</a
+                    >
+                  </div>
                 </div>
               </div>
             </div>
@@ -196,20 +211,40 @@
                 />
                 <div class="card-body p-4">
                   <p class="text-primary mb-0">호스팅</p>
-                  <h4 class="font-weight-bold card-title">
-                    GitHub Page
-                  </h4>
+                  <h4 class="font-weight-bold card-title">GitHub Page</h4>
                   <p class="card-text">
-                    Nullam id dolor id nibh ultricies vehicula ut id elit. Cras
-                    justo odio, dapibus ac facilisis in, egestas eget quam.
-                    Donec id elit non mi porta gravida at eget metus.
+                    깃허브에서는 정적 사이트 호스트를 지원합니다. 그래서 별도의
+                    서버가 필요없습니다.
                   </p>
+                  <a
+                    href="https://github.com/INlg1/deu_food_gui"
+                    class="btn btn-secondary"
+                    ><i class="bi bi-github"></i> 페이지 방문</a
+                  >
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+    </article>
+    <article style="background-color: #9FC088">
+        <section class="py-4 py-xl-5">
+          <div class="container">
+            <div class="text-center p-4 p-lg-5">
+              <h1 class="end_title">
+                Enjoy! GitHub
+              </h1>
+              <p class="end_sub">이때까지 깃허브에 대해서 알아보았어요.이제 한번 써보도록 할까요?</p>
+              <a
+                class="btn btn-primary btn-lg mr-2 py-2 px-4"
+                role="button"
+                href="https://github.com/"
+                >Github</a
+              >
+            </div>
+          </div>
+        </section>
     </article>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+Vue.config.productionTip = false
+
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./assets/css/style.css"
 
-Vue.config.productionTip = false
+
 
 import VueTyperPlugin from 'vue-typer'
 Vue.use(VueTyperPlugin)
+
+import VAnimateCss from 'v-animate-css';
+Vue.use(VAnimateCss);
 
 new Vue({
   render: h => h(App),
